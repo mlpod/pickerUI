7385e80e8094447cc2ef4a3736807c9326d61427
jiaoxuewei <jiaoxuewei@zuoyebang.com>
2021-07-01 20:00:30 +0800

    first commit

--- setup.py
+++ setup.py
@@ -5,14 +5,19 @@
 @Author  : Jiaoxuewei
 @Email   : jovae@qq.com
 """
-
+import os
 from setuptools import setup, find_packages
 
+def fread(fname):
+    filepath = os.path.join(os.path.dirname(__file__), fname)
+    with open(filepath, 'r') as fp:
+        return fp.read()
 setup(
     name = 'pickerUI',
-    version = '1.0.0',
+    version = '1.2.0',
     keywords='pickerUI',
     description = 'a library of menu ui',
+    long_description=fread('README.rst'),
     license = 'MIT License',
     url = 'https://github.com/mlpod/pickerUI',
     author = 'Jiaoxuewei',
